5dbb09cd58a3ecf350b2daf3c5fca310e9f4a32c
maoyue <2367066073@qq.com>
2019-04-04 18:56:33 +0800

    样式微调&删除功能%修改功能优化&我的页面

--- pages/mine/mine.js
+++ pages/mine/mine.js
@@ -5,23 +5,14 @@ Page({
    * 页面的初始数据
    */
   data: {
-    username:'',//用户名称
-    userAvatar:'', //用户头像
+   
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    // this.login()
-    // 查看是否授权
-    // wx.getSetting({
-    //   success(res) {
-    //     if (res.authSetting['scope.userInfo']) {
-    //       this.getUser()
-    //     }
-    //   }
-    // })
+   
   },
   login(){
     let _this = this
@@ -38,8 +29,7 @@ Page({
                 key: 'TOKEN',
                 data:res.data.token,
               })
-              //获取用户信息
-              _this.getUser()
+
             }
           })
         }
@@ -47,29 +37,7 @@ Page({
     })
     
   },
-  // 获取用户信息
-  getUser(){
-    let _this = this;
-    wx.getUserInfo({
-      success(res) {
-        const userInfo = res.userInfo
-        _this.data.username = userInfo.nickName
-        _this.data.userAvatar = userInfo.avatarUrl
-        const gender = userInfo.gender // 性别 0：未知、1：男、2：女
-        const province = userInfo.province
-        const city = userInfo.city
-        const country = userInfo.country
-        console.log(_this.data.userAvatar)
-        _this.setData({
-          username: userInfo.nickName,
-          userAvatar: userInfo.avatarUrl,
-        });
-      }
-    })
-  },
-  bindGetUserInfo(e) {
-    console.log(e.detail.userInfo)
-  },
+  
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
